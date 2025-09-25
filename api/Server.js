44d685a5ef312c04225011a086cdad0ccd2511be
afrizal-10 <adijuliyantoafrizal10@gmail.com>
2025-09-25 09:40:59 +0700

    fix bug domain frontend

--- api/Server.js
+++ api/Server.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://afrizalportfolio.vercel.app/"],
+    origin: ["http://localhost:5173", "https://afrizalportfolio.vercel.app"],
   })
 );
 app.use(express.json());
